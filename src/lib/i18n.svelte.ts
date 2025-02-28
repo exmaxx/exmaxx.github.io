73b4c63881c7072b7b377e8002d25fdc0a83f1ea
Marek Ulicny <xulicny@gmail.com>
2025-02-28 11:48:30 +0100

    fix state reference warning

--- src/lib/i18n.svelte.ts
+++ src/lib/i18n.svelte.ts
@@ -4,9 +4,8 @@ import { DEFAULT_LANG } from '$lib/constants'
 
 type Translations = Record<Lang, Record<string, string>>
 
-export default (translations: Translations) =>
-	(key: string): string => {
-		const currentLang = $derived(page.params.lang || DEFAULT_LANG) as Lang
+export default (translations: Translations) => {
+	const currentLang = $derived(page.params.lang || DEFAULT_LANG) as Lang
 
-		return translations[currentLang][key]
-	}
+	return (key: string) => translations[currentLang][key]
+}
