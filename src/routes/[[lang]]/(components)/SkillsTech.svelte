a5c6b180bdbc0ae6ca0b2b1d2a27fdf7f55d566c
Marek Ulicny <xulicny@gmail.com>
2025-03-21 15:25:18 +0100

    add responsive design to skills

--- src/routes/[[lang]]/(components)/SkillsTech.svelte
+++ src/routes/[[lang]]/(components)/SkillsTech.svelte
@@ -6,12 +6,13 @@
 		<li class="badge">Tailwind 4</li>
 		<li class="badge">Flex</li>
 		<li class="badge">Grid</li>
+		<li class="badge">responsive design</li>
 		<li class="badge">Jest</li>
+		<li class="badge">TDD</li>
 		<li class="badge">refactoring</li>
 		<li class="badge">deep code reviews</li>
 		<li class="badge">Git</li>
 		<li class="badge">Web Vitals</li>
 		<li class="badge">Azure Devops</li>
-		<li class="badge">TDD</li>
 	</ul>
 </section>
